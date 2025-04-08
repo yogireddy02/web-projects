9f72c3fe4c64d4c8a5a55011792c5b687fcfc453
yogireddy02 <121396308+yogireddy02@users.noreply.github.com>
2025-04-08 12:17:32 +0530

    Delete EmployeeLeaveSystem-PHP directory

--- EmployeeLeaveSystem-PHP/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-        <div class="footer-area">
-                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By SYS </p>
-        </div>
-</footer>
